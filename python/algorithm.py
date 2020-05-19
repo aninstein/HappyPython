dd08b4a096371f26e022e4a1f5a3670131926ba4
aninstein@163.com <13132208596@qq.com>
2020-05-20 03:22:00 +0800

    添加了test_level部分代码

--- python/algorithm.py
+++ python/algorithm.py
@@ -3,10 +3,25 @@
 
 
 import time
+import datetime
+
+
+class CountTime(object):
+    def __init__(self):
+        pass
+
+    def __call__(self, func):
+        def inner(self, *args):
+            start_time = datetime.datetime.now()
+            func()
+            over_time = datetime.datetime.now()
+            self.total_time = (over_time - start_time).total_seconds()
+            return self.total_time
+        return inner
 
 
 class Algorithm(object):
-    MAX_LEVEL = 16
+    MAX_LEVEL = 20
 
     def __init__(self, func):
         self.func = func
@@ -18,47 +33,60 @@ class Algorithm(object):
         self.total_time = 0
         self.total_space = 0
 
-        self.test_source_data = None
-        self.exec_result_data = None
-        self.real_answer_data = None
+        self.test_data = None
+        self.result_data = None
+        self.answer_data = None
 
-    def get_data_source(self, **kwargs):
+        self.data_score = 0
+        self.time_score = 0
+        self.space_score = 0
+        self.total_score = 0
+        self.max_score = 0
+
+    def get_data_source(self, *args, **kwargs):
         """
         get the test source
-        :param data_len
         :return:
         """
         pass
 
-    def get_real_answer_data(self):
+    def get_real_answer_data(self, *args, **kwargs):
         """
         real answer algorithm function
         :return:
         """
         pass
 
-    def test_level(self, level=1):
-        """
-        :param level: 1-16
-        :return:
-        """
-        pass
-
-    def exec_algorithm(self):
+    def exec_algorithm(self, *args, **kwargs):
         """
         exec you algorithm, then
         :return:
         """
         pass
 
-    def get_algorithm_level(self):
+    def get_algorithm_level(self, *args, **kwargs):
         """
         get the algorithm is level
         :return:
         """
         pass
 
-    def set_algorithm_standard(self):
+    def test_level(self, *args, **kwargs):
+        """
+        :return:
+        """
+        pass
+
+    def check_time(self, *args, **kwargs):
+        pass
+
+    def check_space(self, *args, **kwargs):
+        pass
+
+    def check_data(self, *args, **kwargs):
+        pass
+
+    def set_algorithm_standard(self, *args, **kwargs):
         """
         set the algorithm level standard data, like:
         [{
@@ -72,21 +100,21 @@ class Algorithm(object):
         """
         pass
 
-    def finish(self):
+    def finish(self, *args, **kwargs):
         """
         when algorithm finish, and reckon test score
         :return:
         """
         pass
 
-    def reckon_test(self):
+    def reckon_test(self, *args, **kwargs):
         """
         reckon test score, include total time, total space, level
         :return:
         """
         pass
 
-    def print_result(self):
+    def print_result(self, *args, **kwargs):
         """
         print result
         :return:
--- python/learn_sort.py
+++ python/learn_sort.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import math
 import numpy as np
+import sys
+import operator
 
-from python.algorithm import Algorithm
+from python.algorithm import Algorithm, CountTime
 
 
 class SortTest(Algorithm):
@@ -12,28 +13,76 @@ class SortTest(Algorithm):
     def __init__(self, func):
         super(SortTest, self).__init__(func)
 
+    def set_algorithm_standard(self):
+        cfg = {}
+        for i in range(1, self.MAX_LEVEL + 1):
+            cfg[i] = {
+                "level": i,
+                "total_time": 100,  # S
+                "total_space": 1024,  # KB
+                "low": 1,
+                "high": int(i / 2) * 100,
+                "data_len": pow(2, i) * 10
+            }
+        self.algorithm_standard_cfg = cfg
+        self.max_score = self.MAX_LEVEL * 5
+
     def get_data_source(self, low=1, high=10, data_len=10):
-        self.test_source_data = np.random.randint(low, high, size=data_len)
-        return self.test_source_data
+        self.test_data = np.random.randint(low, high, size=data_len)
+        return self.test_data
 
     def get_real_answer_data(self):
-        self.real_answer_data = sorted(self.test_source_data)
-        return self.real_answer_data
+        self.answer_data = sorted(self.test_data)
+        return self.answer_data
 
-    def set_algorithm_standard(self):
-        cfg = []
-        for i in range(1, self.MAX_LEVEL + 1):
-            cfg.append({
-                "level": i,
-                "total_time": 100,
-                "total_space": 1000,
-                "data_len": pow(2, i) * 10
-            })
-        self.algorithm_standard_cfg = cfg
+    @CountTime()
+    def exec_algorithm(self):
+        self.result_data = self.func(self.test_data)
+        return self.result_data
 
+    def get_algorithm_level(self):
+        pass
 
+    def test_level(self, level=1):
+        value = self.algorithm_standard_cfg[level]
+        self.get_data_source(low=value.get("low"),
+                             high=value.get("high"),
+                             data_len=value.get("data_len"))
 
+        self.exec_algorithm()
 
+        if operator.eq(self.result_data, self.answer_data):
+            self.data_score += 3
+        else:
+            self.data_score = self.check_data(self.result_data, self.answer_data)
 
+        if self.data_score == 0:
+            return 0
 
+        self.total_space = sys.getsizeof(self) / 1024
+        if self.total_space <= value.get("total_space"):
+            self.space_score += 1
+
+        if self.total_time <= value.get("total_time"):
+            self.time_score += 1
+
+        self.total_score = self.data_score + self.time_score + self.space_score
+        return self.total_score
+
+    def check_data(self, result, answer):
+        if len(result) != len(answer):
+            return 0
+        data_len = len(result)
+        for i in range(data_len):
+            if result[i] == answer[i]:
+                continue
+
+            if i <= data_len * (1 / 3):
+                return 0
+            elif i <= data_len * (2 / 3):
+                return 1
+            elif i <= data_len:
+                return 2
+        else:
+            return 3
 

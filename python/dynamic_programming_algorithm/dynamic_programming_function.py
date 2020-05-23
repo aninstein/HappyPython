d9b93ccdc7946faa557df13c98c56761990d6b91
aninstein@163.com <13132208596@qq.com>
2020-05-24 04:26:25 +0800

    添加动态规划算法题，给出01背包题目

---
+++ python/dynamic_programming_algorithm/dynamic_programming_function.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import random
+import numpy as np
+
+
+def bag_0and1(data):
+    """
+    01背包问题
+    问题描述：有n件物品和容量为m的背包 给出i件物品的重量以及价值 求解让装入背包的物品重量不超过背包容量 且价值最大 。
+    特点:这是最简单的背包问题，特点是每个物品只有一件供你选择放还是不放。
+    :param data:
+    :return:
+    """
+    things_number = 10
+    data = create_random_bag_data(things_number)
+    print(data)
+    total_weight = data.get("total_weight")
+    items = data.get("items")
+    number = data.get("things_num")
+    max_value = _one_dim_function(items, number, total_weight)
+    print(max_value)
+
+
+def _one_dim_function(data, number, total_weight):
+    row = number + 1
+    col = total_weight + 1
+    total_val = np.array([0] * col)
+    for i in range(1, row):
+        if i == len(data):
+            break
+        item = data[i]
+        for j in range(col-1, -1, -1):
+            value = item.get("value")
+            weight = item.get("weight")
+            if weight <= j:
+                input_val = total_val[j - weight] + value
+                total_val[j] = max(input_val, total_val[j])
+    return total_val[total_weight - 1]
+
+
+def _two_dim_function(data, number, total_weight):
+    row = number + 1
+    col = total_weight + 1
+    total_val = np.array([0] * (row * col)).reshape(row, col)
+    for i in range(1, row):
+        if i == len(data):
+            break
+        item = data[i]
+        for j in range(1, col):
+            value = item.get("value")
+            weight = item.get("weight")
+            if weight > j:
+                total_val[i][j] = total_val[i-1][j]
+            else:
+                input_val = total_val[i-1][j-weight] + value
+                noput_val = total_val[i-1][j]
+                total_val[i][j] = max(input_val, noput_val)
+    return total_val[number-1][total_weight]
+
+
+def bag_complete(data):
+    """
+    完全背包问题
+    问题描述：有n件物品和容量为m的背包 给出i件物品的重量以及价值 求解让装入背包的物品重量不超过背包容量 且价值最大 。
+    特点：题干看似与01一样 但它的特点是每个物品可以无限选用。
+    :param data:
+    :return:
+    """
+    pass
+
+
+def bag_multiple(data):
+    """
+    多重背包问题
+    问题描述：有n件物品和容量为m的背包 给出i件物品的重量以及价值 还有数量 求解让装入背包的物品重量不超过背包容量 且价值最大 。
+    特点 ：它与完全背包有类似点 特点是每个物品都有了一定的数量。
+    :param data:
+    :return:
+    """
+    pass
+
+
+def create_random_bag_data(things_num, bag_type="01"):
+    items = []
+    for i in range(things_num):
+        if bag_type == "multiple":
+            number = random.randint(1, 50)
+        elif bag_type == "complete":
+            number = 999999
+        else:
+            number = 1
+        items.append({
+            "number": number,
+            "weight": random.randint(1, 100),
+            "value": random.randint(1, 1000)
+        })
+    total_weight = random.randint(150, 350)
+    return {
+        "items": items,
+        "total_weight": total_weight,
+        "things_num": things_num
+    }
+
+
+if __name__ == '__main__':
+    bag_0and1("aa")
---
+++ python/learn_dynamic_programming.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import sys
+import operator
+
+from algorithm import Algorithm, CountTime
+from copy import deepcopy
+
+
+class DynamicProgramming(Algorithm):
+
+    def __init__(self, func):
+        super(DynamicProgramming, self).__init__(func)
+        self.temp_test_data = None
+
+    def get_data_source(self, low=1, high=10, data_len=10):
+        self.test_data = np.random.randint(low, high, size=data_len)
+        self.test_data = list(self.test_data)
+        self.get_real_answer_data()
+        return self.test_data
+
+    @CountTime()
+    def get_real_answer_data(self):
+        self.answer_data = list(sorted(self.test_data))
+        self.answer_algorithm_time = self.total_time
+        self.answer_algorithm_space = sys.getsizeof(self) / 1024
+        return self.answer_data
+
+    @CountTime()
+    def exec_algorithm(self):
+        self.result_data = self.func(self.temp_test_data)
+        return self.result_data
+
+    def get_algorithm_level(self):
+        for i in range(1, self.MAX_LEVEL + 1):
+            score = self.test_level(level=i)
+            if score == 0:
+                self.finish()
+                break
+            self.reckon_test()
+        self.print_result()
+
+    def test_level(self, level=1):
+        value = self.algorithm_standard_cfg[level]
+        self.get_data_source(low=value.get("low"),
+                             high=value.get("high"),
+                             data_len=value.get("data_len"))
+
+        self.temp_test_data = deepcopy(self.test_data)
+        self.exec_algorithm()
+        if operator.eq(list(self.result_data), list(self.answer_data)):
+            self.data_score += 3
+        else:
+            self.data_score = self.check_data(self.result_data, self.answer_data)
+
+        if self.data_score == 0:
+            return 0
+
+        self.total_space = sys.getsizeof(self) / 1024
+        if self.total_space <= value.get("total_space"):
+            self.space_score += 1
+
+        if self.total_time <= value.get("total_time"):
+            self.time_score += 1
+
+        self.total_score = self.data_score + self.time_score + self.space_score
+        return self.total_score
+
+    def check_data(self, result, answer):
+        if len(result) != len(answer):
+            return 0
+        data_len = len(result)
+        for i in range(data_len):
+            if result[i] == answer[i]:
+                continue
+
+            if i <= data_len * (1 / 3):
+                return 0
+            elif i <= data_len * (2 / 3):
+                return 1
+            elif i <= data_len:
+                return 2
+        else:
+            return 3
+
+    def reckon_test(self):
+        self.level += 1
+        self.data_percent = round(float(self.total_score / self.max_score), 3)
+        self.time_percent = round(float(self.time_score / self.time_max_score), 3)
+        self.space_percent = round(float(self.space_score / self.space_max_score), 3)
+
+    def print_result(self):
+        print(">>>>>>>>> algorithm result <<<<<<<<<")
+        print("maximal test data: ", self.test_data)
+        print("################################")
+        print("last answer data: ", self.answer_data)
+        print("last answer time: ", self.answer_algorithm_time)
+        print("last answer space: ", self.answer_algorithm_space)
+        print("################################")
+        print("last result data: ", self.result_data)
+        print("last total time: ", self.total_time)
+        print("last total space: ", self.total_space)
+        print("\n")
+        print("*** result ***")
+        print("*** algorithm right percent: {0}%".format(self.data_percent * 100))
+        print("*** time right percent: {0}%".format(self.time_percent * 100))
+        print("*** space right percent: {0}%".format(self.space_percent * 100))
+        print("*** result, you algorithm level: %s ***" % self.level)
+        print(">>>>>>>>>>>>>>>> end <<<<<<<<<<<<<<<<")
+
+
+if __name__ == '__main__':
+    pass

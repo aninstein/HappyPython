b15c1e06c53e4641542cd08b177c84732233c063
aninstein@163.com <13132208596@qq.com>
2020-08-10 01:38:24 +0800

    使用go语言

--- golang/algorithm_sort/function_sort.go
+++ golang/algorithm_sort/function_sort.go
@@ -1,7 +1,27 @@
-package algorithm_sort
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello world!")
+    data := []int{1, 5, 7, 1, 8, 9, 4, 2, 3}
+    fmt.Println(bubbleSort(data))
+}
+
+
+func bubbleSort(data []int) []int{
+    if data == nil || len(data) == 0 {
+        return nil
+    }
+
+    dataLen := len(data)
+    for i := 0; i < dataLen; i++{
+        for j := i + 1; j < dataLen; j++{
+            if data[i] > data[j] {
+                tmp := data[i]
+                data[i] = data[j]
+                data[j] = tmp
+            }
+        }
+    }
+    return data
 }

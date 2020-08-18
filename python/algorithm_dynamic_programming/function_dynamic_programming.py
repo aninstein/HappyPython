b3011019b4bcbd16149df09ceec358162650ee38
aninstein@163.com <13132208596@qq.com>
2020-08-19 01:26:38 +0800

    完全背包优化

--- python/algorithm_dynamic_programming/function_dynamic_programming.py
+++ python/algorithm_dynamic_programming/function_dynamic_programming.py
@@ -96,7 +96,7 @@ def bag_complete(data):
     total_weight = data.get("total_weight")
     items = data.get("items")
     number = data.get("things_num")
-    print(_complete_two_dim_k_function(items, number, total_weight))
+    print(_complete_two_dim_function(items, number, total_weight))
 
 
 def _complete_two_dim_k_function(data, number, total_weight):
@@ -131,6 +131,38 @@ def _complete_two_dim_k_function(data, number, total_weight):
     return dp[number - 1][total_weight]
 
 
+def _complete_two_dim_function(data, number, total_weight):
+    """
+    时间复杂度优化，去除了k循环
+    状态转移方程：
+    dp[i][j] = max(dp[i][j-w[i]] + v[i], dp[i-1][j])
+    :param data:
+    :param number:
+    :param total_weight:
+    :return:
+    """
+    # 由于数据从1开始计算因此+1
+    row = number + 1
+    col = total_weight + 1
+    # dp = [[0] * col for _ in range(row)]
+    dp = np.array([0] * (row * col)).reshape(row, col)
+    for i in range(1, row):
+        if i == len(data):
+            break
+        item = data[i]
+        v = item.get("value")
+        w = item.get("weight")
+        for j in range(w, col):
+            if j >= w:
+                input_val = dp[i][j - w] + v
+                noput_val = dp[i - 1][j]
+                dp[i][j] = max(input_val, noput_val)
+            else:
+                dp[i][j] = dp[i - 1][j]
+                break  # 如果w >= j了这个循环就没必要继续了
+    return dp[number - 1][total_weight]
+
+
 def _complete_one_dim_function(data, number, total_weight):
     """
     状态转移方程：
@@ -150,10 +182,8 @@ def _complete_one_dim_function(data, number, total_weight):
         if i == len(data):
             break
         item = data[i]
-        # 这个地方需要是从后面到前面，因为如果从前面到后面的话，会把低层级的j给覆盖掉，
-        # https://www.cnblogs.com/qie-wei/p/10160169.html
-        # 其实是由于状态转移方程为：dp[k](新值) = max(value[i]+dp[k-weight[i]](旧值), dp[k](旧值))
-        # 这个k-weight[i]，如果从前往后数的话，这个值就会被上个值更新为新的值而出现错误
+        # 这个地方需要增序列遍历，原因是因为完全背包算法实际上是需要进行自我比对的
+        # 也就是说我当前的这一轮可以被重复，也就是说一个物品可以放进去多次
         v = item.get("value")
         w = item.get("weight")
         for j in range(w, col):

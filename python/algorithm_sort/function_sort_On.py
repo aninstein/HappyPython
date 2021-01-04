c123a7064b0c9f5213d8f3b4bd905927f5f504fb
lichangan@hp <lichangan@163.com>
2021-01-05 00:29:56 +0800

    基数排序

--- python/algorithm_sort/function_sort_On.py
+++ python/algorithm_sort/function_sort_On.py
@@ -48,19 +48,32 @@ def radix_sort(data):
 
     if not data:
         return []
-    max_num = max(data)
-    max_digit = len(str(abs(max_num)))
+    max_num = max(data)  # 获取当前数列中最大值
+    max_digit = len(str(abs(max_num)))  # 获取最大的位数
 
     dev = 1  # 第几位数，个位数为1，十位数为10···
     mod = 10  # 求余数的除法
     for i in range(max_digit):
-        radix_queue = {[] for k in range(mod * 2)}  # 考虑到负数，我们用两倍队列
+        radix_queue = [list() for k in range(mod * 2)]  # 考虑到负数，我们用两倍队列
+        for j in range(len(data)):
+            radix = int(((data[j] % mod) / dev) + mod)
+            radix_queue[radix].append(data[j])
+
+        pos = 0
+        for queue in radix_queue:
+            for val in queue:
+                data[pos] = val
+                pos += 1
+
         dev *= 10
         mod *= 10
+    return data
 
 
 if __name__ == "__main__":
-    # data = [1, 4, 5, 1, 5, 8, 7, 5, 9, 6, 7, 4, 5, 6, 7, 0]
+    # count_data = [1, 4, 5, 1, 5, 8, 7, 5, 9, 6, 7, 4, 5, 6, 7, 0]
     # print(count_sort(data))
-    lis = [3.1, 4.2, 3.3, 3.5, 2.2, 2.7, 2.9, 2.1, 1.55, 4.456, 6.12, 5.2, 5.33, 6.0, 2.12]
-    print(bucket_sort(lis))
+    # bucket_data = [3.1, 4.2, 3.3, 3.5, 2.2, 2.7, 2.9, 2.1, 1.55, 4.456, 6.12, 5.2, 5.33, 6.0, 2.12]
+    # print(bucket_sort(bucket_data))
+    radix_data = [58, 14, 5, 16, 78, 2, 123, 158, 753, 32, 1, 9, 5]
+    print(radix_sort(radix_data))
